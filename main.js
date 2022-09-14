fba6fef36d0c4dad3785ea0f42e93aa3018479b3
BambooSword <herodreamdeng@gmail.com>
2022-09-14 15:05:16 +0800

    use store to store data

--- main.js
+++ main.js
@@ -1,72 +1,96 @@
-const { app, BrowserWindow, Menu } = require("electron");
-const log = require("electron-log");
+const { app, BrowserWindow, Menu } = require('electron')
+const log = require('electron-log')
+const { ipcMain } = require('electron/main')
 
+const Store = require('./Store')
 // Set env
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = 'development'
 
-const isDev = process.env.NODE_ENV !== "production" ? true : false;
-const isMac = process.platform === "darwin" ? true : false;
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
 
-let mainWindow;
+let mainWindow
+
+// init store & defaults
+
+const store = new Store({
+  configName: 'user-settings',
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+})
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    title: "SysTop",
+    title: 'SysTop',
     width: isDev ? 800 : 355,
     height: 500,
     icon: `${__dirname}/assets/icons/icon.png`,
     resizable: isDev ? true : false,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
-  });
+  })
 
   if (isDev) {
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools()
   }
 
-  mainWindow.loadFile("./app/index.html");
+  mainWindow.loadFile('./app/index.html')
 }
 
-app.on("ready", () => {
-  createMainWindow();
+app.on('ready', () => {
+  createMainWindow()
 
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
-});
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.webContents.send('settings:get', store.get('settings'))
+  })
+
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+})
 
 const menu = [
-  ...(isMac ? [{ role: "appMenu" }] : []),
+  ...(isMac ? [{ role: 'appMenu' }] : []),
   {
-    role: "fileMenu",
+    role: 'fileMenu',
   },
   ...(isDev
     ? [
         {
-          label: "Developer",
+          label: 'Developer',
           submenu: [
-            { role: "reload" },
-            { role: "forcereload" },
-            { type: "separator" },
-            { role: "toggledevtools" },
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
           ],
         },
       ]
     : []),
-];
+]
 
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   if (!isMac) {
-    app.quit();
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow();
+    createMainWindow()
   }
-});
+})
 
-app.allowRendererProcessReuse = true;
+app.allowRendererProcessReuse = true
+
+// Set settings
+ipcMain.on('settings:set', (e, value) => {
+  store.set('settings', value)
+  mainWindow.webContents.send('settings:get', store.get('settings'))
+})
